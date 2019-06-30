d22d323d802e5660afdd368d5904a65b2e364878
Zach Palamara <zach.palamara@gmail.com>
2019-06-30 03:26:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
-  var = i
-  for (i)
+  var i; 
+  for (i = 0, i<katzDeliLine.length; i++){
   return (katzDeliLine.length>0 ? `The line is currently: ${katzDeliLine}`: "The line is currently empty.")
-}
+  }
 
 // You can write:
 // var i;
