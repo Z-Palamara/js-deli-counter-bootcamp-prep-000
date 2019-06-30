44174697f760ee4704a1e94496637fd704aeba76
Zach Palamara <zach.palamara@gmail.com>
2019-06-30 03:20:53 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+katzDeliLine = []
+function takeANumber(katzDeliLine,name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  return (katzDeliLine.length>0 ? `Currently serving ${katzDeliLine.shift()}.`:"There is nobody waiting to be served!")
+}
+
+function currentLine(katzDeliLine){
+  return (katzDeliLine.length>0 ? `The line is currently: ${katzDeliLine}`: "The line is currently empty.")
+}
