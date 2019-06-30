8da4475396feb98f5e4c28e0885529e833d5f91a
Zach Palamara <zach.palamara@gmail.com>
2019-06-30 04:18:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ if(katzDeliLine.length>0){
   let a = ""
   let i;
   let b = ""
-    for (i=0;i<katzDeliLine.length;i++);{
+    for (i=0;i<katzDeliLine.length;i++){
       a = ` ${i+1}. ${katzDeliLine[i]}`
       newArray.push(a)
       }
